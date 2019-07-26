951c9132cb67393378df7a0d23479b6134265fb7
Arta Farahmand <arta.farahmand@gmail.com>
2019-07-26 16:54:00 -0600

    Defined the jasmine tests for menue is hidden and the menu toggling on and off.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -53,20 +53,38 @@ $(function() {
              expect(feed.name.length).not.toBe(0);
            }
          });
+       });
 
     /* TODO: Write a new test suite named "The menu" */
-
+    
+     // Code reference Matthew Cranford URL: https://matthewcranford.com/feed-reader-walkthrough-part-3-menu-test-suite/
+    describe('the menu', function(){
+      it('is hidden', function() {
+        const body = document.querySelector('body');
+        expect(body.classList.contains('menu-hidden')).toBe(true);
+      });
+      
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+         
+         it('toggle on and off', function() {
+           const body = document.querySelector('body');
+           const menu = document.querySelector('.menu-icon-link');
+           
+           menu.click();
+           expect(body.classList.contains('menu-hidden')).toBe(false);
+         });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+          
+          });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
